be519fd0cefa681143b100d2adbfad26a89b902d
nrakochy <nick.rakochy@gmail.com>
2015-03-11 16:37:43 -0500

    Reads URI parameters and parses + spec

--- lib/server_response.rb
+++ lib/server_response.rb
@@ -1,11 +1,14 @@
 require 'uri'
+require 'cgi'
 
 class ServerResponse
   def initialize(request)
     @method = request["method"]
     @uri = request["uri"]
+    @path = @uri.path
     @incoming_data = request["incoming_data"]
     @public_dir = File.expand_path("../../public", __FILE__)
+    @request_params = find_query_params
   end
 
   def interpret_request
@@ -16,63 +19,94 @@ class ServerResponse
       head
     when "POST"
       post
+    when "PUT"
+      put
     when "OPTIONS"
       options
+    when "DELETE"
+      delete
     else
-      raise_404_error
+      raise_error(404, "File not found")
     end
-
-    #put(request["uri"]) if request["method"] == "PUT"
-    #delete(request["uri"]) if request["method"] == "DELETE"
   end
 
   def get
-    path = @uri.path
-    path = '/index.html' if path == "/"
-    full_path = File.join(@public_dir, path)
+    path = check_for_root(@path)
+    full_path = set_full_path(path)
     if legitimate_file_request?(full_path)
       response_body = read_file(full_path)
-      content_type = find_content_type(full_path)
-      header_info = { "status_code" => "200 OK", "content_type" => content_type, "content_length" => response_body.length }
+      header_info = { "status_code" => set_response_message(200), "content_type" => find_content_type(full_path), "content_length" => response_body.length }
       header = create_response_header(header_info)
       { "header" => header, "response_body" => response_body }
     else
-      raise_404_error
+      raise_error(404, "File not found#{@request_params}")
     end
   end
 
   def head
-    path = @uri.path
-    full_path = File.join(@public_dir, path)
+    full_path = set_full_path(@path)
     if legitimate_file_request?(full_path)
       response_body = ""
       content_type = find_content_type(full_path)
-      header_info = { "status_code" => "200 OK", "content_type" => content_type, "content_length" => response_body.length }
+      header_info = { "status_code" => set_response_message(200), "content_type" => content_type, "content_length" => response_body.length }
       header = create_response_header(header_info)
       { "header" => header, "response_body" => response_body }
     else
-      raise_404_error
+      raise_error(404, @request_params)
     end
   end
 
   def post
-    path = @uri.path
-    if path == "/form"
-      response_body = "Your requested data has been received: " + @incoming_data
-      header_info = { "status_code" => "200 OK", "content_type" => "plain/text", "content_length" => response_body.length }
+    if @path == "/form"
+      full_path = set_full_path(@path)
+      write_file(full_path, @incoming_data)
+      response_body = read_file(full_path)
+      header_info = { "status_code" => set_response_message(200), "content_type" => "plain/text", "content_length" => response_body.length }
       header = create_response_header(header_info)
       { "header" => header, "response_body" => response_body }
     else
-      raise_404_error
+      raise_error(405)
     end
+  end
+
+  def put
+    #NOTE: The PUT method is idempotent, whereas POST is not. This simple implementation is not using the incoming data
+    #for anything, so this is written as so to eliminate redundancy, but is simply a placeholder at this point.
+    #Essentially, PUT would overwrite server-side resource underlying "/form", whereas POST would pile it on.
+    post
   end
 
   def options
     additional_information = "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n"
     body = ""
-    header_info = { "status_code" => "200 OK", "content_type" => "text/plain", "content_length" => body.length }
+    header_info = { "status_code" => set_response_message(200), "content_type" => "text/plain", "content_length" => body.length }
     header = create_response_header(header_info, additional_information)
     { "header" => header, "response_body" => body }
+  end
+
+  def delete
+    if @path == "/form"
+      full_path = set_full_path(@path)
+      write_file(full_path, "")
+      response_body = read_file(full_path)
+      header_info = { "status_code" => set_response_message(200), "content_type" => "plain/text", "content_length" => response_body.length }
+      header = create_response_header(header_info)
+      { "header" => header, "response_body" => response_body }
+    else
+      raise_error(400)
+    end
+  end
+
+  def check_for_root(path)
+    (path == "/") ? "/index.html" : path
+  end
+
+  def set_full_path(path)
+    File.join(@public_dir, path)
+  end
+
+  def redirect_uri_path
+    @uri.path = "/"
   end
 
   def read_file(full_path)
@@ -82,8 +116,13 @@ class ServerResponse
     data
   end
 
+  def write_file(full_path, data)
+    File.open(full_path, "wb"){ |file| file.puts data }
+  end
+
   def legitimate_file_request?(requested_file_path)
-    File.exists?(requested_file_path) && !File.directory?(requested_file_path)
+    root = "/"
+    requested_file_path == root || (File.exists?(requested_file_path) && !File.directory?(requested_file_path))
   end
 
   def find_content_type(path)
@@ -102,35 +141,53 @@ class ServerResponse
 
   def create_response_header(response, additional_information ="")
     response = ("HTTP/1.1 #{response["status_code"]}\r\n" +
-               "Date: #{Time.now.to_s}\r\n" +
-               "Content-Type: #{response["content_type"]}\r\n" +
-               "Content-Length: #{response["content_length"]}\r\n")
+                "Date: #{Time.now.to_s}\r\n" +
+    "Content-Type: #{response["content_type"]}\r\n" +
+    "Content-Length: #{response["content_length"]}\r\n")
     response + additional_information
   end
 
-  def raise_404_error
-    message = "File not found"
-    header_info = { "status_code" => "404 Not Found", "content_type" => "text/plain", "content_length" => message.length }
+  def raise_error(status_code, error_message = "")
+    response_code = set_response_message(status_code)
+    response_body = response_code + " " + error_message
+    header_info = { "status_code" => response_code, "content_type" => "text/plain", "content_length" => response_body.length }
     header = create_response_header(header_info)
-    { "header" => header, "response_body" => message }
+    { "header" => header, "response_body" => response_body }
   end
 
   def set_response_message(status_code)
     response_messages = {
-      "200" => "OK",
-      "201" => "Created",
-      "204" => "No Content",
-      "301" => "Moved Permanently",
-      "302" => "Moved Temporarily",
-      "304" => "Not Modified",
-      "400" => "Bad Request",
-      "401" => "Unauthorized",
-      "403" => "Forbidden",
-      "404" => "Not Found",
-      "500" => "Internal Server Error"
+      200 => "200 OK",
+      201 => "201 Created",
+      204 => "204 No Content",
+      301 => "301 Moved Permanently",
+      302 => "302 Moved Temporarily",
+      304 => "304 Not Modified",
+      400 => "400 Bad Request",
+      401 => "401 Unauthorized",
+      403 => "403 Forbidden",
+      404 => "404 Not Found",
+      405 => "405 Method Not Allowed",
+      500 => "500 Internal Server Error"
     }
-    response_messages.default = "Not Found"
     response_messages[status_code]
   end
 
+  def find_query_params
+    if @uri.query.nil?
+      ""
+    else
+      params = CGI::parse(@uri.query)
+      convert_queries_to_string(params)
+    end
+  end
+
+  def convert_queries_to_string(queries)
+    results = queries.map do |key, value|
+      combined = value.join(" ")
+      " #{key} = #{combined}\n"
+    end
+    puts results
+    results.join('')
+  end
 end
--- spec/server_response_spec.rb
+++ spec/server_response_spec.rb
@@ -1,52 +1,121 @@
 require 'server'
 
-describe HTTPServer do
+describe ServerResponse do
 
-  let(:legit_post_req){ { "method" => "POST", "uri" => URI("/form"), "incoming_data" => "params1=value1" } }
+  let(:legit_post_req){ { "method" => "POST", "uri" => URI("/form?variable_1=Operators%2"), "incoming_data" => "params1=value1" } }
   let(:basic_post_response){ ServerResponse.new(legit_post_req) }
 
-  describe "#create_response_header" do
-    it "returns a properly formatted HTTP response header" do
-      response =
-        { "status_code" => "200 OK", "content_type" => "text/html", "content_length" => "25" }
-      expected_result =
-        "HTTP/1.1 200 OK\r\n" +
-        "Date: #{Time.now.to_s}\r\n" +
+  context "Interpretation Methods: #get / #head" do
+    describe "#legitimate_file_request?" do
+      it 'returns false if requested file path is a directory' do
+        filepath = File.expand_path("../", __FILE__)
+        expect(basic_post_response.legitimate_file_request?(filepath)).to eq(false)
+        expect(File.directory?(filepath)).to eq(true)
+      end
+
+      it 'returns false if requested file path leads to non-existing file' do
+        filepath = File.expand_path("../non_existent.txt", __FILE__)
+        expect(basic_post_response.legitimate_file_request?(filepath)).to eq(false)
+      end
+
+      it 'returns true if requested file exists' do
+        filepath = File.expand_path("../../public/text-file.txt", __FILE__)
+        expect(basic_post_response.legitimate_file_request?(filepath)).to eq(true)
+      end
+
+      it 'returns true if get request is for the root directory' do
+        root = "/"
+        expect(basic_post_response.legitimate_file_request?(root)).to eq(true)
+      end
+    end
+  end
+
+  context "Response Builder Methods" do
+    describe "#create_response_header" do
+      it "returns a properly formatted HTTP response header" do
+        response =
+          { "status_code" => "200 OK", "content_type" => "text/html", "content_length" => "25" }
+        expected_result =
+          "HTTP/1.1 200 OK\r\n" +
+          "Date: #{Time.now.to_s}\r\n" +
         "Content-Type: text/html\r\n" +
-        "Content-Length: 25\r\n"
-      expect(basic_post_response.create_response_header(response)).to eq(expected_result)
-    end
-  end
-
-  describe "#legitimate_file_request?" do
-    it 'returns false if requested file path is a directory' do
-      filepath = File.expand_path("../", __FILE__)
-      expect(basic_post_response.legitimate_file_request?(filepath)).to eq(false)
-      expect(File.directory?(filepath)).to eq(true)
+          "Content-Length: 25\r\n"
+        expect(basic_post_response.create_response_header(response)).to eq(expected_result)
+      end
     end
 
-    it 'returns false if requested file path leads to non-existing file' do
-      filepath = File.expand_path("../non_existent.txt", __FILE__)
-      expect(basic_post_response.legitimate_file_request?(filepath)).to eq(false)
-    end
-
-    it 'returns true if requested file exists' do
-      filepath = File.expand_path("../../public/text-file.txt", __FILE__)
-      expect(basic_post_response.legitimate_file_request?(filepath)).to eq(true)
-    end
-  end
-
-  describe "#interpret_request" do
-    it "returns a hash with properly formatted response_header and response body" do
-      bogus_request = { "method" => "GET", "uri" => URI("/path/to/file/index.html"), "incoming_data" => "params1=value1" }
-      expected_404_message = "File not found"
-      expected_404_header = (
-        "HTTP/1.1 404 Not Found\r\n" +
-        "Date: #{Time.now.to_s}\r\n" +
+    describe "#raise_error" do
+      it "returns a hash with header and response_body based on status code with
+        status code as response_body if no message given" do
+        error = basic_post_response.raise_error(404)
+        expected_header =
+          "HTTP/1.1 404 Not Found\r\n" +
+          "Date: #{Time.now.to_s}\r\n" +
         "Content-Type: text/plain\r\n" +
-        "Content-Length: 14\r\n")
-      expect(ServerResponse.new(bogus_request).interpret_request).to eq(
-        { "header" => expected_404_header, "response_body" => expected_404_message })
+          "Content-Length: 14\r\n"
+        expect(error["header"]).to eq(expected_header)
+        expect(error["response_body"]).to eq("404 Not Found ")
+      end
+
+      it "returns a hash with header and response_body based on status code with
+        status code + message given as response_body" do
+        error = basic_post_response.raise_error(404, "File not found")
+        expected_header =
+          "HTTP/1.1 404 Not Found\r\n" +
+          "Date: #{Time.now.to_s}\r\n" +
+        "Content-Type: text/plain\r\n" +
+          "Content-Length: 28\r\n"
+        expect(error["header"]).to eq(expected_header)
+        expect(error["response_body"]).to eq("404 Not Found File not found")
+      end
+    end
+  end
+
+  context "HTTP Response Integration methods" do
+    describe "#interpret_request" do
+      it "returns a hash with properly formatted response_header and response body" do
+        bogus_request = { "method" => "GET", "uri" => URI("/path/to/file/index.html"), "incoming_data" => "params1=value1" }
+        expected_404_message = "404 Not Found File not found"
+        expected_404_header = (
+          "HTTP/1.1 404 Not Found\r\n" +
+          "Date: #{Time.now.to_s}\r\n" +
+          "Content-Type: text/plain\r\n" +
+            "Content-Length: 28\r\n")
+        expect(ServerResponse.new(bogus_request).interpret_request).to eq(
+          { "header" => expected_404_header, "response_body" => expected_404_message })
+      end
+    end
+  end
+  context "Helper Methods" do
+    describe "#find_query_params" do
+      it "returns a hash of query parameters from a URI with parameters" do
+        post_req = { "method" => "POST", "uri" => URI("/form?variable_1=Operators%2"), "incoming_data" => "params1=value1" }
+        queried = ServerResponse.new(post_req).find_query_params
+        expect(queried).to eq(" variable_1 = Operators%2\n")
+      end
+
+      it "returns a hash of query parameters from a URI with parameters" do
+        post_req = { "method" => "POST", "uri" => URI("/form"), "incoming_data" => "params1=value1" }
+        queried = ServerResponse.new(post_req).find_query_params
+        expect(queried).to eq("")
+      end
+    end
+
+    describe "#convert_query_to_string" do
+      it "returns a strings with format param=query from single query" do
+        post_req = { "method" => "POST", "uri" => URI("/form?variable_1=Operators%2"), "incoming_data" => "params1=value1" }
+        response = ServerResponse.new(post_req)
+        query = { "variable_1" => ["Operators%2"] }
+        expect(response.convert_queries_to_string(query)).to eq(" variable_1 = Operators%2\n")
+      end
+
+      it "returns a strings with format param=query from multiple queries" do
+        post_req = { "method" => "POST", "uri" => URI("/form?variable_1=Operators%2variable_2=Operators%3"), "incoming_data" => "params1=value1" }
+        response = ServerResponse.new(post_req)
+        query = { "variable_1" => ["Operators%2"], "variable_2" =>["Operators%3"] }
+        expect(response.convert_queries_to_string(query)).to eq(" variable_1 = Operators%2\n variable_2 = Operators%3\n")
+      end
+
     end
   end
 end
